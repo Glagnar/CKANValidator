7ad86c23034cd9e96b9ac5bb068120820b71a862
Thomas Gilbert <thomas.gilbert@alexandra.dk>
2015-03-16 10:38:36 +0100

    Updated help

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # CKANValidator
 Force validation of resources
+
+IMPORTANT:
+
+Set: ckan.auth.create_unowned_dataset = false
+
+# The username below will be granted access to publish datasets
+Set: ckan.setstateforpendingvalidation.user = glagnar
+
+How 
+
+import urllib2
+import urllib
+import json
+import pprint
+
+# Put the id of the dataset here
+dataset_dict = {
+        'id': '5e4ed304-3183-4c09-a606-41f09eadaf5f',
+        'private': False,
+}
+
+# Use the json module to dump the dictionary to a string for posting.
+data_string = urllib.quote(json.dumps(dataset_dict))
+
+# Use the URL of the instance here
+request = urllib2.Request('http://localhost:5000/api/action/package_update')
+
+# Updating a dataset requires an authorization header.
+# Replace *** with your API key, from your user account specified for the user at ckan.setstateforpendingvalidation.user
+# that you're creating the dataset on.
+request.add_header('Authorization', '***')
+
+# Make the HTTP request.
+response = urllib2.urlopen(request, data_string)
+
+assert response.code == 200
+
+# Use the json module to load CKAN's response into a dictionary.
+response_dict = json.loads(response.read())
+assert response_dict['success'] is True
+
+# package_create returns the created package as its result.
+created_package = response_dict['result']
+pprint.pprint(created_package)
--- ckanext/setstateforpendingvalidation/plugin.py
+++ ckanext/setstateforpendingvalidation/plugin.py
@@ -1,4 +1,5 @@
 import logging
+import pylons.config as config
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
@@ -8,40 +9,29 @@ log = logging.getLogger(__name__)
 class SetStateForPendingValidationPlugin(plugins.SingletonPlugin):
    plugins.implements(plugins.IPackageController, inherit=True)
 
-   def after_create(self, context, data_dict):
-    log.info("CKAN after_create self: %r", self)
-    log.info("CKAN after_create context: %r", context)
-    log.info("CKAN after_create data_dict: %r", data_dict)
-    
-    aboutToUpdate = {
-            'private': True,
-    }
-
-    if 'id' not in data_dict:
-        pass
-
-    aboutToUpdate['id'] = data_dict['id']
-    if 'state' not in data_dict or ( 'state' in data_dict and data_dict['state'] == 'active'):
-        if 'private' not in data_dict or ( 'private' in data_dict and data_dict['private'] == False):
-            log.info("CKAN to send %r: ", aboutToUpdate)
-            toolkit.get_action('package_update')(context, aboutToUpdate)
-
-
    def after_update(self, context, data_dict):
-    log.info("CKAN after_update self: %r", self)
-    log.info("CKAN after_update context: %r", context)
-    log.info("CKAN after_update data_dict: %r", data_dict)
+    log.debug("CKAN after_update context: %r", context)
+    log.debug("CKAN after_update data_dict: %r", data_dict)
     
+    # Check if the user is the one who is allowed to publish datasets
+    userAllowedToPublish = config.get('ckan.setstateforpendingvalidation.user')
+    if userAllowedToPublish == context['user']:
+        log.info("User is allowed to publish dataset")
+        return
+
+    # We only need to set private to True for the dataset id specified
     aboutToUpdate = {
             'private': True,
     }
-
-    if 'id' not in data_dict:
-        pass
-
     aboutToUpdate['id'] = data_dict['id']
+
+    # Only do this for active datasets, and not ones in draft or deleted state
+    # it is also not needed if the dataset is already private
     if 'state' not in data_dict or ( 'state' in data_dict and data_dict['state'] == 'active'):
         if 'private' not in data_dict or ( 'private' in data_dict and data_dict['private'] == False):
             log.info("CKAN to send %r: ", aboutToUpdate)
             toolkit.get_action('package_update')(context, aboutToUpdate)
-
+   
+   # Update and create should do the same
+   def after_create(self, context, data_dict):
+    self.after_update(context, data_dict)

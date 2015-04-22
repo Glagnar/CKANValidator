06f8384817e7674f66da13ef27d68079abdefbb2
root <root@CKAN.(none)>
2015-04-22 14:03:59 +0200

    Update for potential error

--- ckanext/setstateforpendingvalidation/plugin.py
+++ ckanext/setstateforpendingvalidation/plugin.py
@@ -20,17 +20,14 @@ class SetStateForPendingValidationPlugin(plugins.SingletonPlugin):
         return
 
     # We only need to set private to True for the dataset id specified
-    aboutToUpdate = {
-        'private': True,
-    }
-    aboutToUpdate['id'] = data_dict['id']
 
     # Only do this for active datasets, and not ones in draft or deleted state
     # it is also not needed if the dataset is already private
     if 'state' not in data_dict or ( 'state' in data_dict and data_dict['state'] == 'active'):
         if 'private' not in data_dict or ( 'private' in data_dict and data_dict['private'] == False):
-            log.info("CKAN to send %r: ", aboutToUpdate)
-            toolkit.get_action('package_update')(context, aboutToUpdate)
+  	    data_dict['private'] = True
+            log.info("CKAN to send %r: ", data_dict)
+            toolkit.get_action('package_update')(context, data_dict)
    
    # Update and create should do the same
    def after_create(self, context, data_dict):
